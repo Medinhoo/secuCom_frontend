176375f7d3f335b3b229069396a8bb78b67856a2
Mehdi <chihi.med@gmail.com>
2025-03-08 02:26:53 +0100

    security: setting env variables

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 // URL de base de l'API
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api";
+const API_URL = import.meta.env.SECUCOM_API;
 
 // Types
 export interface User {
